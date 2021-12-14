4d1edce0d9b4c90566872a1cceb26eb56ea8d2a2
YU Bin <yubenoit@gmail.com>
2021-12-14 18:56:03 +0100

    添加中文注释

--- src/run.bash
+++ src/run.bash
@@ -3,7 +3,7 @@
 # export DISPLAY=:0
 
 # Xvfb $DISPLAY -screen 0 1920x1080x24+32 &
-
+# watchdog.bash文件用于监测worker运行状态 每分钟检测一次 输出“python /src/run_worker.py lean_and_mean python /src/run.py ”
 /src/worker/watchdog.bash python /src/run_worker.py lean_and_mean &
 
 python /src/run.py $@
--- src/worker/watchdog.bash
+++ src/worker/watchdog.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 while true; do
+# 打印跟在本文件后面的命令行参数 本文件在run.bash中被调用
     echo "starting: $@"
     eval $@
     sleep 60
